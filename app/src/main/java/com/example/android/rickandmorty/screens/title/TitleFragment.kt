054a8bd20cc0bd06648137ada7a8e0c98b5812b9
Marcio de Freitas Nascimento <59565829+marcionascimentonb@users.noreply.github.com>
2021-02-26 19:43:33 -0400

    done

--- app/src/main/java/com/example/android/rickandmorty/screens/title/TitleFragment.kt
+++ app/src/main/java/com/example/android/rickandmorty/screens/title/TitleFragment.kt
@@ -1,12 +1,12 @@
 package com.example.android.rickandmorty.screens.title
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.rickandmorty.R
 import com.example.android.rickandmorty.databinding.FragmentTitleBinding
 
@@ -26,7 +26,20 @@ class TitleFragment : Fragment() {
         binding.playGameButton.setOnClickListener {
             findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
+        setHasOptionsMenu(true)
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.
+        onNavDestinationSelected(item,requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+
 
 }
