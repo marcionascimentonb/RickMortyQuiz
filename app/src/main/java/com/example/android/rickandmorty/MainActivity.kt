5f834e0777a14b3916e95b8036013084438f51e5
Marcio de Freitas Nascimento <59565829+marcionascimentonb@users.noreply.github.com>
2021-02-25 22:33:54 -0400

    progress

--- app/src/main/java/com/example/android/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/example/android/rickandmorty/MainActivity.kt
@@ -1,11 +1,45 @@
-package com.example.android.rickandmorty
+//package com.example.android.rickandmorty
+//
+//import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+//
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+//}
+//
 
-import androidx.appcompat.app.AppCompatActivity
+package com.example.android.navigation
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.rickandmorty.R
+import com.example.android.rickandmorty.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+
+        val navController = this.findNavController(R.id.navHostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+
 }
